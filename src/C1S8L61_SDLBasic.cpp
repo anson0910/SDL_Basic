a16f7c5b8d10ede788fbea907e6a841c421c9340
Anson <anson32330200@gmail.com>
2017-01-13 11:27:01 -0800

    feat: Set all pixels in window to white

--- src/C1S8L61_SDLBasic.cpp
+++ src/C1S8L61_SDLBasic.cpp
@@ -23,7 +23,7 @@ int main() {
 	cout << "SDL init successful.\n";
 
 	SDL_Window *window = SDL_CreateWindow("Particle Explosion",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
 			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
 	if (window == NULL) {
@@ -31,6 +31,36 @@ int main() {
 		cout << "SDL window creation failed.\n";
 		return 2;
 	}
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(renderer,
+			SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH,
+			SCREEN_HEIGHT);
+
+	if (renderer == NULL) {
+		cout << "Could not create renderer.\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if (texture == NULL) {
+		cout << "Could not create texture.\n";
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+	memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
 
 	SDL_Event event;
 	bool quit = false;
@@ -42,6 +72,9 @@ int main() {
 		}
 	}
 
+	delete [] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
