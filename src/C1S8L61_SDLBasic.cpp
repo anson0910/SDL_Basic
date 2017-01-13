394bffbd42db27b6c260963479a54a3aed273a1d
Anson <anson32330200@gmail.com>
2017-01-13 10:57:25 -0800

    feat: Create simple window that listens to quit events

--- src/C1S8L61_SDLBasic.cpp
+++ src/C1S8L61_SDLBasic.cpp
@@ -13,12 +13,36 @@ using namespace std;
 
 int main() {
 
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL init failed.\n";
 		return 1;
 	}
 	cout << "SDL init successful.\n";
 
+	SDL_Window *window = SDL_CreateWindow("Particle Explosion",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (window == NULL) {
+		SDL_Quit();
+		cout << "SDL window creation failed.\n";
+		return 2;
+	}
+
+	SDL_Event event;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
