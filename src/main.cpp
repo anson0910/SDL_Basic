3e798f5b3c057acf3e5ca23192a639725be04be1
Anson <anson32330200@gmail.com>
2017-01-13 16:49:22 -0800

    feat: Change colors gradually

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <Math.h>
 
 #include "Screen.h"
 
@@ -24,10 +25,13 @@ int main() {
 
 	while (true) {
 		// set particles
+		int elapsed = SDL_GetTicks();
+		Uint8 red = (Uint8) ((1 + sin(elapsed * 0.002)) * 128);
+		Uint8 green = (Uint8) ((1 + sin(elapsed * 0.001)) * 128);
+		Uint8 blue = (Uint8) ((1 + sin(elapsed * 0.003)) * 128);
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++)
 			for (int x = 0; x < Screen::SCREEN_WIDTH; x++)
-				screen.setPixel(x, y, 0x80, 0, 0xFF);
-		screen.setPixel(400, 300, 0xFF, 0xFF, 0xFF);
+				screen.setPixel(x, y, red, green, blue);
 		// update particles
 		screen.update();
 		// check events
